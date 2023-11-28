d557d2572f61995c30c30697766fe4f2a33f46e0
zyhou <richard.maxime9@gmail.com>
2023-11-28 11:24:35 +0100

    add tmux and nvim config

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add catppuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha", -- latte / mocha
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  -- Configure https://github.com/nvim-neo-tree/neo-tree.nvim
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+    },
+  }
+}
