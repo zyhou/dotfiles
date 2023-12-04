cfab7926222687250c587cf8d3d8a25891af944c
zyhou <richard.maxime9@gmail.com>
2023-12-04 21:37:11 +0100

    customize noice.nvim

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,15 @@
+return {
+  -- https://github.com/folke/noice.nvim
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        hover = {
+          -- Set not show a message if hover is not available
+          -- ex: shift+k on Typescript code
+          silent = true,
+        },
+      },
+    },
+  },
+}
