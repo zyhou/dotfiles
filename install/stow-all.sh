f40578e205544a0964c91bafdad8554d2fdae1c7
zyhou <richard.maxime9@gmail.com>
2023-12-06 11:08:04 +0100

    fix stow script

--- install/stow-all.sh
+++ install/stow-all.sh
@@ -4,6 +4,6 @@ echo '[*] Removing default configurations ...'
 rm -rf ~/.config/alacritty ~/.config/bat ~/.config/Code/User/settings.json ~/.gitconfig ~/.config/neofetch/config.conf ~/.config/nvim ~/.config/starship.toml ~/.tmux.conf ~/.zshrc
 
 echo '[*] Stowing/Creating simlinks'
-cd .. & stow -vSt ~ .fonts .icons .themes alacritty bat code git neofetch nvim starship tmux zsh
+cd .. && stow -vSt ~ .fonts .icons .themes alacritty bat code git neofetch nvim starship tmux zsh
 
 fc-cache -f -v
