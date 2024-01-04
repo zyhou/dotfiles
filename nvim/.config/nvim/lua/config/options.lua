cd0281e2c9a07954b27ac276e42ee89e3d1faf3b
zyhou <richard.maxime9@gmail.com>
2024-01-04 16:06:27 +0100

    improve delta theme + conceallevel to 0

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+-- The default conceallevel is 3
+opt.conceallevel = 0
