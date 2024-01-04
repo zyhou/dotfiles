7e4ba8ad719bb5e90327ef8620844093b86e9aa5
zyhou <richard.maxime9@gmail.com>
2024-01-04 23:34:13 +0100

    add live grep args to telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       "nvim-lua/plenary.nvim",
       "debugloop/telescope-undo.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
     },
     build = "make",
     keys = {
@@ -14,10 +15,25 @@ return {
         "<cmd>Telescope undo<cr>",
         desc = "undo history",
       },
+      {
+        "<leader>.",
+        function()
+          require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h") })
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("telescope").extensions.live_grep_args.live_grep_args()
+        end,
+        desc = "Live Grep (Args)",
+      },
     },
     config = function()
       require("telescope").load_extension("fzf")
       require("telescope").load_extension("undo")
+      require("telescope").load_extension("live_grep_args")
     end,
   },
 }
