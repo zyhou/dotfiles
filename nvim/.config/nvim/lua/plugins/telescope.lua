e78fc341d2d96cd4c3da8c2e8f40a7f4efcf61a7
zyhou <richard.maxime9@gmail.com>
2024-04-25 13:28:17 +0200

    use telescope option instead of custom code for fine name first

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,24 +1,3 @@
--- Having file name at first
--- https://github.com/nvim-telescope/telescope.nvim/issues/2014#issuecomment-1873229658
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "TelescopeResults",
-  callback = function(ctx)
-    vim.api.nvim_buf_call(ctx.buf, function()
-      vim.fn.matchadd("TelescopeParent", "\t\t.*$")
-      vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
-    end)
-  end,
-})
-
-local function filenameFirst(_, path)
-  local tail = vim.fs.basename(path)
-  local parent = vim.fs.dirname(path)
-  if parent == "." then
-    return tail
-  end
-  return string.format("%s\t\t%s", tail, parent)
-end
-
 return {
   -- https://github.com/nvim-telescope/telescope.nvim
   {
@@ -71,10 +50,14 @@ return {
       },
       pickers = {
         find_files = {
-          path_display = filenameFirst,
+          path_display = {
+            "filename_first",
+          },
         },
         git_files = {
-          path_display = filenameFirst,
+          path_display = {
+            "filename_first",
+          },
         },
       },
     },
